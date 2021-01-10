059884d4743dca1bab857a66b18985dbc9defdb6
Rasheduzzaman138 <rshamim965@gmail.com>
2021-01-10 14:12:35 +0600

    updated files

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'https://images.pexels.com/photos/4195342/pexels-photo-4195342.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas Fit Shirt',
+            category:'Shirts',
+            image:'https://images.pexels.com/photos/1661416/pexels-photo-1661416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            price:100,
+            brand:'Adidas',
+            rating:4.0,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+          
+            _id:'3',
+            name:'Lacoste Free Shirt',
+            category:'Shirts',
+            image:'https://images.pexels.com/photos/1043473/pexels-photo-1043473.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            price:220,
+            brand:'Lacoste',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pant',
+            category:'Pant',
+            image:'https://images.pexels.com/photos/1430879/pexels-photo-1430879.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            price:320,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:14,
+            description:'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Puma Slim Pant',
+            category:'Pant',
+            image:'https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            price:220,
+            brand:'Puma',
+            rating:4.0,
+            numReviews:13,
+            description:'high quality product'
+        },
+        {
+            _id:'6',
+            name:'Adidas Fit Pant',
+            category:'Pant',
+            image:'https://images.pexels.com/photos/1766702/pexels-photo-1766702.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            price:420,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:17,
+            description:'high quality product'
+        }
+    ]
+}
+export default data;
